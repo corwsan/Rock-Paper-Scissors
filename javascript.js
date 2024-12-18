e381df8acaf7c8bd07be1e82e26601849bf030e4
corwsan <191419896+corwsan@users.noreply.github.com>
2024-12-18 20:38:10 +1300

    Add playRound function to log both choices

--- javascript.js
+++ javascript.js
@@ -1,35 +1,30 @@
+
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice() {    
-
    let computerChoice = Math.floor(Math.random() * 3)
-
    if (computerChoice == 0)
     return "Rock";
    else if (computerChoice == 1)
     return "Paper"
    else if (computerChoice == 2)
-    return "Scissors"
-    
+    return "Scissors"    
    return computerChoice
 }
 
-function getHumanChoice () {
-
+function getHumanChoice() {
     let humanChoice = prompt("Will you pick Rock, Paper or Scissors?")
     
     return humanChoice;
 }
-
 //console.log("Computer chose: " + getComputerChoice() + "\nYou chose: " + getHumanChoice())
-
-let humanScore = 0
-let computerScore = 0
-
-
 const humanChoice = getHumanChoice()
 const computerChoice = getComputerChoice()
 
-
 function playRound (getHumanChoice, getComputerChoice) {
+    
+
     console.log("You chose: " + humanChoice + "\nComputer Chose " + computerChoice);
 
 }
