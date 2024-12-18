218541afc6e1e4047789fdcc2c06e4493af516e1
corwsan <191419896+corwsan@users.noreply.github.com>
2024-12-18 18:52:11 +1300

    Add function getComputerChoice which returns rock paper or scissors as a string

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("hello world!")
+function getComputerChoice() {    
+   choice = Math.floor(Math.random() * 3)
+   if (choice == 0)
+    choice = "Rock";
+   else if (choice == 1)
+    choice = "Paper"
+   else if (choice == 2)
+    choice = "Scissors"
+    
+   return choice
+}
+
+console.log(getComputerChoice())
