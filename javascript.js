57b1b85cc066bb57e0c01e31e31f8e4586d349b6
corwsan <191419896+corwsan@users.noreply.github.com>
2024-12-19 17:29:04 +1300

    Add function playGame to play 5 rounds of playRound

--- javascript.js
+++ javascript.js
@@ -1,6 +1,43 @@
+/* 
+Make play game function
+Run five rounds of playRound
+Keep track of score across 5 rounds
+Declare winner at the end
+ */
 
-let humanScore = 0
-let computerScore = 0
+/*let humanSelection = getHumanChoice()
+    //console.log(humanSelection)
+let computerSelection = getComputerChoice()
+    //console.log(computerSelection)*/
+let round = 1;
+    console.log("round" +round);
+let humanScore = 0;
+    console.log(humanScore);
+let computerScore = 0;
+    console.log(computerScore);
+
+playGame ();
+
+function playGame () {
+
+    while (round <= 5) {
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log("Round: " + round + "\nHuman Score: " + humanScore + "\nComputer Score : " + computerScore);
+        round++        
+    }
+
+    if (humanScore === computerScore) {
+        console.log("It's a tie! \nHuman Score: " + humanScore + "\nComputer Score: " + computerScore);
+    } else if (humanScore > computerScore) {
+            console.log("You win! \nHuman Score: " + humanScore + "\nComputer Score: " + computerScore);
+        } else {
+            console.log("You lose! \nHuman Score: " + humanScore + "\nComputer Score: " + computerScore);
+        }
+    }
+
+    
+    
+
 
 function playRound (humanChoice, computerChoice) {
 
@@ -11,55 +48,54 @@ function playRound (humanChoice, computerChoice) {
         console.log("It's a tie!");
     } else if (humanChoice === "Rock") {
         if (computerChoice === "Scissors") {
-            console.log("You win! Rock smashes scissors.")
-            humanScore++
+            console.log("You win! Rock smashes scissors.");
+            humanScore++;
         } else {
-            console.log("You lose! Paper covers rock.")
-            computerScore++
+            console.log("You lose! Paper covers rock.");
+            computerScore++;
         }
     } else if (humanChoice === "Paper") {
         if (computerChoice === "Rock") {
-            console.log("You win! Paper covers rock.")
-            humanScore++
+            console.log("You win! Paper covers rock.");
+            humanScore++;
         } else {
-            console.log("You lose! Scissors cut paper.")
-            computerScore++
+            console.log("You lose! Scissors cut paper.");
+            computerScore++;
         }
     } else if (humanChoice === "Scissors") {
         if (computerChoice === "Paper") {
-            console.log("You win! Scissors cut paper.")
-            humanScore++
+            console.log("You win! Scissors cut paper.");
+            humanScore++;
         } else {
-            console.log("You lose! Rock smashes scissors.")
-            computerScore++
+            console.log("You lose! Rock smashes scissors.");
+            computerScore++;
         }
     }
 }  
 
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
 
-playRound(humanSelection, computerSelection)
+
 
 function getComputerChoice() {    
     const computerChoice = Math.floor(Math.random() * 3)
-    if (computerChoice == 0)
-     return "Rock";
-    else if (computerChoice == 1)
+    if (computerChoice == 0) {
+     return "Rock"
+    } else if (computerChoice == 1) {
      return "Paper"
-    else if (computerChoice == 2)
-     return "Scissors"    
+    } else if (computerChoice == 2) {
+     return "Scissors" 
+    }   
     return computerChoice
  }
  
  function getHumanChoice() {
      const humanChoice = prompt("Will you pick Rock, Paper or Scissors?");
-     if (humanChoice.toLowerCase() === "rock")
-        return "Rock";
-    else if (humanChoice.toLowerCase() === "paper")
+     if (humanChoice.toLowerCase() === "rock") {
+        return "Rock"; 
+    } else if (humanChoice.toLowerCase() === "paper") {
         return "Paper";
-    else if (humanChoice.toLowerCase() === "scissors")
+    } else if (humanChoice.toLowerCase() === "scissors")
         return "Scissors"
 }
     
